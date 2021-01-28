ba9e67a0cf2dc2d5f23aff064a62f76a29f23426
luciangrozav <luciangrozav01@gmail.com>
2021-01-28 15:00:05 +0200

    Added car class; studied variables;

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,62 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Ce faci tu?" );
+       Car carReference = new Car(); // prima variabila declarata
+
+       carReference.name = "Ferrari";
+       carReference.maxSpeed=300;
+        carReference.fuellevel=60;
+        carReference.mileage = 12.5;
+        carReference.totalTravelDistance = 0;
+        carReference.damaged = true;
+        carReference.doorCount = 1;
+        carReference.color = "Red";
+
+        //concatenation
+        System.out.println("Properties of car " + carReference.name);
+        System.out.println("Maximum speed is: " + carReference.maxSpeed);
+        System.out.println("Fuel level is: " + carReference.fuellevel);
+        System.out.println("Mileage: " + carReference.mileage);
+        System.out.println("Total travel distance: " + carReference.totalTravelDistance);
+        System.out.println("Damage: " + carReference.damaged);
+        System.out.println("Door count: " + carReference.doorCount);
+        System.out.println("Color: " + carReference.color);
+        System.out.println(carReference);
+
+        Car car2 = new Car(); // o noua variabila
+        car2.name = "Renault";
+        car2.fuellevel = 70;
+        car2.totalTravelDistance = 100;
+
+        System.out.println("Properties of car " + car2.name);
+        System.out.println("Maximum speed is: " + car2.maxSpeed);
+        System.out.println("Fuel level is: " + car2.fuellevel);
+        System.out.println("Mileage: " + car2.mileage);
+        System.out.println("Total travel distance: " + car2.totalTravelDistance);
+        System.out.println("Damage: " + car2.damaged);
+        System.out.println("Door count: " + car2.doorCount);
+        System.out.println("Color: " + car2.color);
+
+     System.out.println("Initial name for car1 " + carReference.name);
+        carReference.name = "Audi";
+     System.out.println("Changed name for car1 " + carReference.name);
+
+     carReference = new Car();
+     carReference.name = "BMW";
+     System.out.println("New name for car1 " + carReference.name);
+
+
+     carReference = car2;
+     System.out.println("Name of car referenced from 2 variables; carReference " + carReference.name);
+     System.out.println("Name of car referenced from 2 variables; car2 " + car2.name);
+
+     car2.name = "Dacia";
+
+     System.out.println("Name of car referenced from 2 variables; carReference " + carReference.name);
+     System.out.println("Name of car referenced from 2 variables; car2 " + car2.name);
+
+     car2 = null;
+
+     System.out.println(carReference.name);
     }
 }
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+public class Car {
+    String name;
+
+    double fuellevel;
+    double mileage;
+    double totalTravelDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+
+}
