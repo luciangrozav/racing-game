dbeb6657fa9dd0b09b4f9545e35225e547a22a41
luciangrozav <luciangrozav01@gmail.com>
2021-02-19 15:57:34 +0200

    Reading track selected by user;

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -14,6 +14,9 @@ public class Game{
 
         initializeTracks();
 
+        Track selectedTrack = getSelectedTrackFromUser(); // pt ca metoda getSelectedTrackFromUser returneaza o variabila Track (din lista array)
+
+        System.out.println("Selected track: " + selectedTrack.getName() + " with the length: " + selectedTrack.getLength());
 
         int playerCount = getPlayerCountFromUser();
 
@@ -41,7 +44,9 @@ public class Game{
     }
 
     private void displayTracks(){
-        //classical for
+
+        System.out.println("Available tracks: ");
+        //classical 'for'
         for(int i=0; i< tracks.length; i++)
             if(tracks[i] != null)  // altfel da eroare la afisare
             {  System.out.println((i+1) + ". " + tracks[i].getName() + ": " + tracks[i].getLength() + " km"); }
@@ -59,6 +64,14 @@ public class Game{
     {
         System.out.println("Please enter vehicle name: ");
         return ScannerUtils.nextLine();
+    }
+
+    private Track getSelectedTrackFromUser()
+    {
+        System.out.println("Please select a track: ");
+        int trackNumber = ScannerUtils.nextIntAndMoveToNextLine();  // citirea din compilator; citire care trebuie sa fie de tip int; 'cin>>' din C++;
+
+        return tracks[trackNumber -1]; // -1 pt ca indexul incepe de la zero in array; returneaza obiectul din tracks, cu toate proprietatile lui; (Name si Length)
     }
 
 }
