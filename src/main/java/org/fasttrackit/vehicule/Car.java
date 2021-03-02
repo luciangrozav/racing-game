72dc7c4debbe3010582ad480bc36db638ad0d88d
luciangrozav <luciangrozav01@gmail.com>
2021-03-02 14:04:33 +0200

    Abstract classes;

--- src/main/java/org/fasttrackit/vehicule/Car.java
+++ src/main/java/org/fasttrackit/vehicule/Car.java
@@ -2,16 +2,14 @@ package org.fasttrackit.vehicule;
 
 // "is-a" relantionship (inheritance)
 
-import org.fasttrackit.Engine;
-
 public class Car extends Vehicle {  // relatia de mostenire; clasa AutoVehicle este clasa parinte a lui Car
 
     private int doorCount;
 
 
-    public Car(Engine engine) {  // oriunde in App.java va fi obligatoriu sa existe un engine, astfel constructorul trebuie apelat la fiecare new (); de tip Car si AutoVehicle
-        super(engine);
-    }
+//    public Car(Engine engine) {  // oriunde in App.java va fi obligatoriu sa existe un engine, astfel constructorul trebuie apelat la fiecare new (); de tip Car si AutoVehicle
+//        super(engine);
+//    }
 
     public int getDoorCount() {
         return doorCount;
